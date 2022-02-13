87cb764f6a0ea4e2689853d6d92269d9e55232d9
weslley <weslleyborges34@gmail.com>
2022-02-13 16:02:56 -0300

    commit 2

--- número_de_quartos.php
+++ número_de_quartos.php
@@ -15,7 +15,8 @@
       <input type="number" name="número_quartos" value=""> <br/>
       Preço por quarto. <br/>
       <input type="number" step="0.01" name="preço_quartos" value=""> <br/>
-      <input style="margin: 10px;" type="submit" name="enviar">
+      <input style="margin: 10px;" type="submit" name="enviar" value="Criar">
+      <input type="submit" name="quantidade" value="Quantidade de quartos">
     </form>
   </body>
 </html>
@@ -24,26 +25,33 @@
 
   if(isset($_POST['enviar'])) {
 
-  $_SESSION['número_quartos'] = $_POST['número_quartos'];
+  $número_quartos = $_POST['número_quartos'];
   $preço_quartos = $_POST['preço_quartos'];
 
-  while ($i <= $_SESSION['número_quartos']) {
-    echo "<br/> quarto de número $i <br/>";
-        $query = mysqli_query($conect, $comando);
-    $i++;
-  }
-
-}else{
-      $_SESSION['número_quartos'] = null;
-      $preço_quartos = null;  }
-
-    $comando = "insert into quartos values (default, 0, $preço_quartos)";
+    $comando = "insert into quartos values (default, default, $preço_quartos)";
 
     $i = 1;
 
-    while ($i <= $_SESSION['número_quartos']) {
-      echo "<br/> quarto de número $i <br/>";
+    while ($i <= $número_quartos) {
           $query = mysqli_query($conect, $comando);
+          if ($query == 1) {
+              echo "<br/> quarto de número $i Criado com sucesso<br/>";
+          }else{  echo "ERRO ao criar o quarto de número $i";  }
       $i++;
     }
+  }
+
+  if (isset($_POST['quantidade'])) {
+    $quantidade_quartos = "SELECT * FROM quartos";
+    $query = mysqli_query($conect, $quantidade_quartos);
+    $num_rows = mysqli_num_rows($query);
+    echo "Existe no total <b style = 'font-size: 20px;'> $num_rows </b> quartos.";
+  }
+
+/*
+    $quantidade_quartos = "SELECT * FROM quartos";
+    $query = mysqli_query($conect, $quantidade_quartos);
+    $num_rows = mysqli_num_rows($query);
+    echo "$num_rows";
+*/
 ?>
---
+++ teste.php
@@ -0,0 +1,17 @@
+<?php
+  function FunctionName()
+  {
+    echo "olá mundo";
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <button type="button" name="button" onclick="FunctionName();">Clique</button>
+  </body>
+</html>
